f1a9ad98fd1458ef005685d0cd2eb0a97d0164ab
mavantgarderc <manihabibinava@gmail.com>
2025-10-23 05:57:25 +0330

    chore(.gitignore)

--- lua/testfiles/luatest.lua
+++
@@ -1,9 +0,0 @@
-local n = tonumber(arg and arg[1]) or 10
-local a, b = 0, 1
-io.write("fib: ")
-for i = 1, n do
-  io.write(a)
-  if i < n then io.write(", ") end
-  a, b = b, a + b
-end
-io.write("\n")
