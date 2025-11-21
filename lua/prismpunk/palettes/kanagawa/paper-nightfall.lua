de842a0f81d4de6411a6d7d0c7ae47ffaac06e81
mavantgarderc <manihabibinava@gmail.com>
2025-11-21 06:56:48 +0330

    fix(colorscheme): kanagawa-paper-nightfall

--- lua/prismpunk/palettes/kanagawa/paper-nightfall.lua
+++ lua/prismpunk/palettes/kanagawa/paper-nightfall.lua
@@ -7,21 +7,25 @@ return {
 
   fujiWhite = "#DCD7BA",
   oldWhite = "#C8C093",
+  softGray = "#727169",
+
   winterRed = "#43242B",
   winterGreen = "#2B3328",
+  winterYellow = "#49443C",
+
   autumnRed = "#C34043",
   autumnGreen = "#76946A",
   autumnYellow = "#DCA561",
-  waveBlue1 = "#465e7f",
+
+  waveBlue1 = "#465E7F",
   waveBlue2 = "#2D4F67",
   waveAqua1 = "#6A9589",
+
   springViolet1 = "#938AA9",
   springViolet2 = "#9CABCA",
-  winterYellow = "#49443C",
 
   heroicCyan = "#7FB4CA",
   gentlePink = "#D27E99",
   calmTeal = "#7AA89F",
   subtleGold = "#C0A36E",
-  softGray = "#727169",
 }
