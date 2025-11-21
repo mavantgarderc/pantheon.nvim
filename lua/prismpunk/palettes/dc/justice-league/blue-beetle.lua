f0226e88812325ad1af8468d758f3886c35fdffd
mavantgarderc <manihabibinava@gmail.com>
2025-11-22 02:38:26 +0330

    colorscheme(dc): justice-league-blue-beetle

---
+++ lua/prismpunk/palettes/dc/justice-league/blue-beetle.lua
@@ -0,0 +1,34 @@
+return {
+  bg_darkest = "#030814",
+  bg_darker = "#071022",
+  bg_dark = "#101724",
+  bg_mid = "#14202a",
+  bg_light = "#23303a",
+  bg_lighter = "#2d3d4a",
+  bg_lightest = "#384a5a",
+
+  fg_lightest = "#dff7f7",
+  fg_light = "#a6dfff",
+  fg_mid = "#9fb7c9",
+  fg_dark = "#53656f",
+
+  bg_alt1 = "#0a1420",
+  bg_alt2 = "#050a14",
+  bg_alt3 = "#0f1a28",
+  bg_alt4 = "#040810",
+
+  scarabCore = "#2b5fbf",
+  scarabGlow = "#33ffff",
+  armorBlue = "#1866cc",
+  techTeal = "#66e6d9",
+  scarabCyan = "#33ffff",
+
+  reachLime = "#99ffcc",
+  alienGold = "#ffd966",
+  cosmicPink = "#ff99cc",
+  techSilver = "#a6dfff",
+  khajidaRage = "#ff6666",
+
+  warningOrange = "#ffc870",
+  infoBlue = "#61afef",
+}
