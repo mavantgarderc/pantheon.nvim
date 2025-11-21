2fea6512d40fc7952cfa3d8cf50e90430e75002d
mavantgarderc <manihabibinava@gmail.com>
2025-11-21 06:57:37 +0330

    fix(colorscheme): kangawa-paper-sunset

--- lua/prismpunk/palettes/kanagawa/paper-sunset.lua
+++ lua/prismpunk/palettes/kanagawa/paper-sunset.lua
@@ -1,19 +1,22 @@
 return {
-  sumiInk0 = "#1b1a23",
-  sumiInk1 = "#22222e",
-  sumiInk2 = "#2e2e3b",
-  sumiInk3 = "#3a3a4a",
-  sumiInk4 = "#4a4a5a",
+  sumiInk0 = "#1B1A23",
+  sumiInk1 = "#22222E",
+  sumiInk2 = "#2E2E3B",
+  sumiInk3 = "#3A3A4A",
+  sumiInk4 = "#4A4A5A",
 
   fujiWhite = "#E5E0C4",
   oldWhite = "#CFC9A2",
   softPaper = "#D8D3B2",
+  mistGray = "#8E8C9C",
 
   autumnRed = "#C34043",
   autumnYellow = "#DCA561",
   autumnGreen = "#76946A",
+
   springViolet1 = "#938AA9",
   springViolet2 = "#9CABCA",
+
   waveAqua1 = "#6A9589",
   waveBlue1 = "#223249",
   waveBlue2 = "#2D4F67",
@@ -23,7 +26,6 @@ return {
   twilightPink = "#D68C8C",
   sandTan = "#C4A484",
   horizonViolet = "#A98BBF",
-  mistGray = "#8E8C9C",
   emberOrange = "#FF9E64",
   flareRose = "#D16B7C",
   emberYellow = "#F6C177",
