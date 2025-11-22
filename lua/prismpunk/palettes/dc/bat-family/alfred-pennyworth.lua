43002d711aabfd12003f83e08e8b09f3d00c151f
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:29:46 +0330

    colorscheme(dc): bat-family-alfred-pennyworth

---
+++ lua/prismpunk/palettes/dc/bat-family/alfred-pennyworth.lua
@@ -0,0 +1,46 @@
+return {
+  bg_darkest = "#060608",
+  bg_darker = "#0b0b0f",
+  bg_dark = "#121218",
+  bg_mid = "#1a1a23",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f1015",
+  bg_alt2 = "#070809",
+  bg_alt3 = "#14151d",
+  bg_alt4 = "#080a0d",
+
+  butlerSuit = "#4b4b4b",
+  wayneTie = "#800020",
+  alfredGray = "#708090",
+  classicGray = "#696969",
+  servantBlack = "#2c2c2c",
+
+  teaTime = "#d4a574",
+  britishTan = "#c19a6b",
+  manorWood = "#8b7355",
+  polishedSilver = "#c0c0c0",
+  vintageBrass = "#b5a642",
+
+  medicKit = "#e63946",
+  firstAid = "#dc3545",
+  medicalWhite = "#f8f9fa",
+  bandageBeige = "#d4c5b9",
+
+  royalBlue = "#4169e1",
+  dignityBlue = "#5b7c99",
+  britishBlue = "#6495ed",
+  butlerTeal = "#5f9ea0",
+
+  wisdomGold = "#daa520",
+  loyaltyAmber = "#ffbf00",
+  serviceGreen = "#8fbc8f",
+  devotionGray = "#a9a9a9",
+}
