5a0de991acc9a2d2f97a695824faf0d092262e39
mavantgarderc <manihabibinava@gmail.com>
2025-11-22 06:34:33 +0330

    colorscheme(dc): justice-league-hawkman

---
+++ lua/prismpunk/palettes/dc/justice-league/hawkman.lua
@@ -0,0 +1,51 @@
+return {
+  bg_darkest = "#090507",
+  bg_darker = "#0f0b0b",
+  bg_dark = "#141215",
+  bg_mid = "#1b1719",
+  bg_light = "#221d1f",
+  bg_lighter = "#2f2a26",
+  bg_lightest = "#3a3532",
+
+  fg_lightest = "#efe7e2",
+  fg_light = "#c4b8af",
+  fg_mid = "#9e9086",
+  fg_dark = "#6e6660",
+
+  bg_alt1 = "#1a1517",
+  bg_alt2 = "#0d0909",
+  bg_alt3 = "#211c1e",
+  bg_alt4 = "#120e10",
+
+  brassBright = "#d9a04a",
+  brassCore = "#b07a2d",
+  brassDark = "#8b5f2b",
+  brassDeep = "#6b4921",
+
+  primalRed = "#d94f4f",
+  warriorRed = "#b33a3a",
+  bloodRed = "#8e2d2d",
+
+  ancientGold = "#ffd966",
+  featherGold = "#ffd9b3",
+  wingBronze = "#8b5f2b",
+
+  skyIndigo = "#5566a3",
+  mysticBlue = "#4a5c99",
+  flightBlue = "#61afef",
+
+  stone = "#9e9086",
+  stoneLight = "#b5a89d",
+  leather = "#5a5252",
+  shadowGray = "#342f2f",
+  dimGray = "#5a5252",
+  lightGray = "#efe7e2",
+
+  dangerRed = "#d94f4f",
+  warningOrange = "#ffc870",
+  infoBlue = "#61afef",
+  hintTeal = "#5fa899",
+
+  earthBrown = "#6b4921",
+  metalGray = "#7a7268",
+}
