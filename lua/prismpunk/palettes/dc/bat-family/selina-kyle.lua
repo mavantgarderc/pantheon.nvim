c54dbd211ab4f8cfa8b0ae5f5e2f5a34a76c15df
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:32:23 +0330

    colorscheme(dc): bat-family-selina-kyle

---
+++ lua/prismpunk/palettes/dc/bat-family/selina-kyle.lua
@@ -0,0 +1,46 @@
+return {
+  bg_darkest = "#0a0508",
+  bg_darker = "#100a0e",
+  bg_dark = "#1a0f14",
+  bg_mid = "#24181f",
+  bg_light = "#3d2d35",
+  bg_lighter = "#56424d",
+  bg_lightest = "#6f5764",
+
+  fg_lightest = "#f5e8f1",
+  fg_light = "#e8d0df",
+  fg_mid = "#c9a8bc",
+  fg_dark = "#9a7588",
+
+  bg_alt1 = "#150c11",
+  bg_alt2 = "#0d070a",
+  bg_alt3 = "#1d101a",
+  bg_alt4 = "#0e080b",
+
+  selinaBlack = "#1a0f14",
+  laceDark = "#2d1f26",
+  velvetBlack = "#241a20",
+  midnightSilk = "#1f151a",
+
+  femmeRed = "#dc143c",
+  lipstickRed = "#e63946",
+  passionRed = "#ff1744",
+  roseRed = "#c41e3a",
+
+  diamondWhite = "#f8f8ff",
+  pearlSheen = "#faf0e6",
+  jewelWhite = "#fffff0",
+  silverShine = "#e8e8f0",
+
+  seductionPink = "#ff69b4",
+  blushPink = "#ffb6c1",
+  rougePink = "#ff1493",
+  mysteryMagenta = "#da70d6",
+
+  emeraldHeist = "#50c878",
+  sapphireBlue = "#0f52ba",
+  amethystPurple = "#9966cc",
+
+  champagneGold = "#f7e7ce",
+  luxuryGold = "#ffd700",
+}
