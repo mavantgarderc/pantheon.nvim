e92e88c7e1a13a966761928d0a24ca6ce7083549
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:31:33 +0330

    colorscheme(dc): bat-family-nightwing

---
+++ lua/prismpunk/palettes/dc/bat-family/nightwing.lua
@@ -0,0 +1,38 @@
+return {
+  bg_darkest = "#050608",
+  bg_darker = "#0a0b0f",
+  bg_dark = "#111217",
+  bg_mid = "#1a1a22",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f1015",
+  bg_alt2 = "#070809",
+  bg_alt3 = "#14151d",
+  bg_alt4 = "#080a0d",
+
+  escrimalBlue = "#00aaff",
+  nightwingBlue = "#1e90ff",
+  blüdhavenBlue = "#4169e1",
+  acrobatCyan = "#00ddff",
+  flyingGrayson = "#56b6c2",
+
+  suitBlack = "#0e0e0e",
+  graysonGray = "#1a1a1a",
+  titanSteel = "#bfa334",
+  wingdingGold = "#d4af37",
+
+  robinGreen = "#00ddaa",
+  dickGraysonTeal = "#00c199",
+  haleyCircus = "#98c379",
+
+  batfamilyRed = "#e06c75",
+  gothamOrange = "#d19a66",
+  bruceWayneBlue = "#61afef",
+}
