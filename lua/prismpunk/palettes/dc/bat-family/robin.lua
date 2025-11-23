95d72500955ad2e86346f161eed9504ad844f8be
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:32:08 +0330

    colorscheme(dc): bat-family-robin

---
+++ lua/prismpunk/palettes/dc/bat-family/robin.lua
@@ -0,0 +1,48 @@
+return {
+  bg_darkest = "#050608",
+  bg_darker = "#0a0b0f",
+  bg_dark = "#111217",
+  bg_mid = "#1a1a22",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f1015",
+  bg_alt2 = "#070809",
+  bg_alt3 = "#14151d",
+  bg_alt4 = "#080a0d",
+
+  robinRed = "#ff0000",
+  damianRed = "#dc143c",
+  capeRed = "#c41e3a",
+  assassinRed = "#b71c1c",
+  bloodRed = "#8b0000",
+
+  tunicGreen = "#00ff00",
+  leagueGreen = "#32cd32",
+  robinGreen = "#228b22",
+  assassinGreen = "#006400",
+
+  katanaSilver = "#c0c0c0",
+  swordSteel = "#a9a9a9",
+  bladeGray = "#808080",
+  armorGold = "#ffd700",
+
+  alGhulGold = "#daa520",
+  leagueOfShadows = "#2f4f4f",
+  raAlGhul = "#556b2f",
+  taliaGreen = "#6b8e23",
+
+  sonOfBatman = "#1e90ff",
+  bruceBlue = "#4169e1",
+  wayneBlue = "#4682b4",
+
+  superiorityPurple = "#9370db",
+  arroganceViolet = "#8b008b",
+  heirOrange = "#ff8c00",
+}
