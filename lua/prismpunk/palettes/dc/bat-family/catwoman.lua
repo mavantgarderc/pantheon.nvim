eff49fe932bcf86719b45e247729b77cb62fe99f
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:30:23 +0330

    colorscheme(dc): bat-family-catwoman\

---
+++ lua/prismpunk/palettes/dc/bat-family/catwoman.lua
@@ -0,0 +1,47 @@
+return {
+  bg_darkest = "#050508",
+  bg_darker = "#0a0a0e",
+  bg_dark = "#101014",
+  bg_mid = "#18181f",
+  bg_light = "#2d2d35",
+  bg_lighter = "#42424d",
+  bg_lightest = "#57575f",
+
+  fg_lightest = "#f5f5f5",
+  fg_light = "#d4d4d8",
+  fg_mid = "#a1a1aa",
+  fg_dark = "#71717a",
+
+  bg_alt1 = "#0e0e12",
+  bg_alt2 = "#070709",
+  bg_alt3 = "#13131a",
+  bg_alt4 = "#08080b",
+
+  catSuit = "#1a1a1a",
+  leatherBlack = "#0d0d0d",
+  midnightBlack = "#0a0a0a",
+  prowlGray = "#a1a1aa",
+  shadowPurr = "#71717a",
+
+  clawSilver = "#c0c0c0",
+  whipSteel = "#a9a9a9",
+  jewelThief = "#e5e5e5",
+  heistGray = "#d3d3d3",
+
+  selinaGreen = "#98fb98",
+  catEyes = "#7fff00",
+  emeraldHeist = "#50c878",
+  envyGreen = "#8fbc8f",
+
+  gothamsRooftop = "#4a5568",
+  alleySmoke = "#6b7280",
+  eastEndFog = "#9ca3af",
+
+  passionPurple = "#da70d6",
+  mysteryViolet = "#9370db",
+  seductionMagenta = "#ba55d3",
+
+  batmanGray = "#708090",
+  bruceWayne = "#4169e1",
+  complicatedLove = "#b0c4de",
+}
