f4ac6b4e7e015c0a9e2005411fae68241e23bdda
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:31:46 +0330

    colorscheme(dc): bat-family-red-hood

---
+++ lua/prismpunk/palettes/dc/bat-family/red-hood.lua
@@ -0,0 +1,45 @@
+return {
+  bg_darkest = "#050608",
+  bg_darker = "#0a0b0f",
+  bg_dark = "#111217",
+  bg_mid = "#1a1a22",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f1015",
+  bg_alt2 = "#070809",
+  bg_alt3 = "#14151d",
+  bg_alt4 = "#080a0d",
+
+  hoodRed = "#ff0000",
+  jasonRed = "#ff1744",
+  crimeAlleyRed = "#f44336",
+  revengeRed = "#e53935",
+  bloodRed = "#b71c1c",
+  crimsonRage = "#dc143c",
+
+  leatherJacket = "#1a1614",
+  outlawBlack = "#0e0e0e",
+  armorGray = "#2d2d2d",
+  antiHeroGray = "#4a4a4a",
+
+  arsenalOrange = "#ff5722",
+  crowbarSteel = "#d32f2f",
+  gunmetalGray = "#ef5350",
+  bulletBrass = "#ff6b6b",
+
+  lazarusPit = "#00ff41",
+  gothamTeal = "#00ddaa",
+  raAlGhul = "#00cc88",
+
+  robinMemory = "#98c379",
+  bruceBetrayal = "#61afef",
+  jokerPurple = "#9370db",
+  talyaGold = "#d4af37",
+}
