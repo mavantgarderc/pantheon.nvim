aaafb102e20eab453b17250d40a78f16c69c51a8
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:31:07 +0330

    colorscheme(dc): bat-family-huntress

---
+++ lua/prismpunk/palettes/dc/bat-family/huntress.lua
@@ -0,0 +1,42 @@
+return {
+  bg_darkest = "#050508",
+  bg_darker = "#0a0a0e",
+  bg_dark = "#0f0f14",
+  bg_mid = "#16161d",
+  bg_light = "#2d2d35",
+  bg_lighter = "#44444d",
+  bg_lightest = "#5a5a64",
+
+  fg_lightest = "#f0f0f5",
+  fg_light = "#d8d8e0",
+  fg_mid = "#b0b0c0",
+  fg_dark = "#808090",
+
+  bg_alt1 = "#0d0d12",
+  bg_alt2 = "#070709",
+  bg_alt3 = "#12121a",
+  bg_alt4 = "#08080b",
+
+  huntressViolet = "#8b00ff",
+  deepPurple = "#6a0dad",
+  royalViolet = "#7b2cbf",
+  darkViolet = "#5a189a",
+  shadedPurple = "#4a148c",
+
+  crossbowWhite = "#ffffff",
+  boltSilver = "#e8e8f0",
+  steelGray = "#c0c0d0",
+  weaponMetal = "#a0a0b0",
+
+  vendettaRed = "#ff0055",
+  bloodCrimson = "#dc143c",
+  rageRed = "#e63946",
+
+  gothamGold = "#ffa500",
+  amberGlow = "#ffb347",
+  orangeFlare = "#ff8c00",
+
+  nightCyan = "#00ffff",
+  huntCyan = "#00e5e5",
+  trackerTeal = "#00cccc",
+}
