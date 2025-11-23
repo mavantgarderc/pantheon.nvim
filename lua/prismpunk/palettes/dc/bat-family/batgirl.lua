6f45bfdf6d441a248814dc5ec5fd0b5ccec1343d
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:30:00 +0330

    colorscheme(dc): bat-family-batgirl

---
+++ lua/prismpunk/palettes/dc/bat-family/batgirl.lua
@@ -0,0 +1,38 @@
+return {
+  bg_darkest = "#060609",
+  bg_darker = "#0b0b0f",
+  bg_dark = "#121218",
+  bg_mid = "#1a1a23",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f0f14",
+  bg_alt2 = "#080809",
+  bg_alt3 = "#16161f",
+  bg_alt4 = "#0a0a0d",
+
+  capeViolet = "#5b2c6f",
+  suitPurple = "#7b3f8f",
+  batgirlPurple = "#9966cc",
+  oraclePurple = "#b794f4",
+  gordonsLight = "#d4af37",
+
+  utilityGold = "#d4af37",
+  batarangGold = "#ffd966",
+  gothamYellow = "#e5c07b",
+
+  clocktowerBlue = "#61afef",
+  barbaraBlue = "#57a7e0",
+  nightGreen = "#00ddaa",
+  gothamTeal = "#56b6c2",
+
+  jokersRed = "#e06c75",
+  felonyPink = "#c678dd",
+  vigilanteOrange = "#d19a66",
+}
