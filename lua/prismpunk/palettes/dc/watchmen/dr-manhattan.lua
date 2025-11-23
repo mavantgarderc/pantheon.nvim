15f0697d0b7559c6be24107b9533e9304555d144
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:35:37 +0330

    colorscheme(dc): watchmen-dr-manhattan

---
+++ lua/prismpunk/palettes/dc/watchmen/dr-manhattan.lua
@@ -0,0 +1,79 @@
+return {
+  bg_darkest = "#05080f",
+  bg_darker = "#0a0f1a",
+  bg_dark = "#0f1524",
+  bg_mid = "#151c2e",
+  bg_light = "#1c2438",
+  bg_lighter = "#242d43",
+  bg_lightest = "#2d364d",
+
+  fg_lightest = "#e6f4ff",
+  fg_light = "#d0e8ff",
+  fg_mid = "#b0d4ff",
+  fg_dark = "#88b0e8",
+
+  bg_alt1 = "#0c111e",
+  bg_alt2 = "#060a12",
+  bg_alt3 = "#121826",
+  bg_alt4 = "#030508",
+
+  quantumBlue = "#00d9ff",
+  quantumBlueBright = "#33e4ff",
+  quantumBlueCore = "#00b8e6",
+  quantumBlueDeep = "#0097cc",
+  quantumBlueDark = "#007aa8",
+
+  nuclearGlow = "#5fdfff",
+  nuclearGlowBright = "#8aeaff",
+  nuclearGlowCore = "#3dd4ff",
+  nuclearGlowDeep = "#1fb8e6",
+
+  atomicCyan = "#00ffff",
+  atomicCyanBright = "#4dffff",
+  atomicCyanCore = "#00e6e6",
+  atomicCyanDeep = "#00cccc",
+
+  hydrogenWhite = "#e6f4ff",
+  hydrogenWhiteBright = "#ffffff",
+  hydrogenWhiteCore = "#d0e8ff",
+  hydrogenWhiteDeep = "#b0d4ff",
+
+  martianRed = "#b8554a",
+  martianRedBright = "#d46f64",
+  martianRedCore = "#a04438",
+  martianRedDeep = "#88362d",
+
+  timelessPurple = "#6b7aaa",
+  timelessPurpleBright = "#8598cc",
+  timelessPurpleCore = "#5a658a",
+  timelessPurpleDeep = "#4a526f",
+
+  omniscientViolet = "#7a6baa",
+  omniscientVioletBright = "#9885cc",
+  omniscientVioletCore = "#655a8a",
+  omniscientVioletDeep = "#52476f",
+
+  detachedGray = "#6b7a8a",
+  detachedGrayLight = "#8598aa",
+  detachedGrayDark = "#5a6570",
+
+  energyField = "#00ccff",
+  energyFieldPulse = "#33d9ff",
+
+  marsRed = "#9a4a42",
+  marsSand = "#c08070",
+  marsRock = "#7a5a52",
+
+  tachyonBlue = "#4d8aff",
+  tachyonBlueBright = "#6fa8ff",
+  tachyonBlueDeep = "#3d6fe6",
+
+  dangerRed = "#d46f64",
+  warningAmber = "#d4a570",
+  infoBlue = "#00b8e6",
+  successGreen = "#4a9a8a",
+
+  quantum = "#00d9ff",
+  omnipotent = "#5fdfff",
+  godComplex = "#00ffff",
+}
