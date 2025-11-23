f06023101ed2d621d3f1b295fba9c3eaa4698f15
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:31:56 +0330

    colorscheme(dc): bat-family-red-robin

---
+++ lua/prismpunk/palettes/dc/bat-family/red-robin.lua
@@ -0,0 +1,42 @@
+return {
+  bg_darkest = "#060609",
+  bg_darker = "#0b0b0e",
+  bg_dark = "#121218",
+  bg_mid = "#1a1a23",
+  bg_light = "#3a3f4c",
+  bg_lighter = "#565c64",
+  bg_lightest = "#6a7178",
+
+  fg_lightest = "#e8ecf0",
+  fg_light = "#c8ccd4",
+  fg_mid = "#abb2bf",
+  fg_dark = "#6a7178",
+
+  bg_alt1 = "#0f0f14",
+  bg_alt2 = "#080809",
+  bg_alt3 = "#14151d",
+  bg_alt4 = "#080a0d",
+
+  timRed = "#ff0000",
+  redRobinRed = "#e63946",
+  detectiveRed = "#dc143c",
+  drakeRed = "#c41e3a",
+  titanRed = "#b71c1c",
+
+  boStaff = "#8b4513",
+  wingSteel = "#d4af37",
+  capeBlack = "#0e0e0e",
+  drakeGray = "#1a1a1a",
+
+  youngJustice = "#61afef",
+  teenTitans = "#00ddaa",
+  wayneBlue = "#4169e1",
+  gothamTeal = "#56b6c2",
+
+  detectiveGold = "#ffd700",
+  clueMasterYellow = "#ffeb3b",
+  evidenceAmber = "#ffa726",
+
+  bruceGray = "#98c379",
+  raAlGhul = "#00ff41",
+}
