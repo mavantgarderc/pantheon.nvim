8a67edadb83c30954642ac6e1aa2a8e5c3e497af
mavantgarderc <manihabibinava@gmail.com>
2025-11-23 03:30:13 +0330

    colorscheme(dc): bat-family-bruce-wayne

---
+++ lua/prismpunk/palettes/dc/bat-family/bruce-wayne.lua
@@ -0,0 +1,42 @@
+return {
+  bg_darkest = "#0a0805",
+  bg_darker = "#100d08",
+  bg_dark = "#1a150f",
+  bg_mid = "#241f18",
+  bg_light = "#3d362b",
+  bg_lighter = "#564d3f",
+  bg_lightest = "#6f6452",
+
+  fg_lightest = "#f5f1e8",
+  fg_light = "#e8dfd0",
+  fg_mid = "#c9bca8",
+  fg_dark = "#9a8b75",
+
+  bg_alt1 = "#15110c",
+  bg_alt2 = "#0d0a07",
+  bg_alt3 = "#1d1810",
+  bg_alt4 = "#0e0b08",
+
+  wayneLibrary = "#8b7355",
+  oldLeather = "#6d5c4d",
+  manorWood = "#5d4e3f",
+  oakDesk = "#4a3f2f",
+
+  scholarGold = "#d4af37",
+  ancientGold = "#b8941f",
+  gilded = "#daa520",
+  antiqueBrass = "#c9a961",
+
+  philosophyBlue = "#4682b4",
+  intellectBlue = "#5b8db8",
+  wisdomBlue = "#6495ed",
+  studyBlue = "#7ba3c4",
+
+  richBurgundy = "#800020",
+  wineRed = "#722f37",
+  mahoganRed = "#c04000",
+
+  archiveGray = "#918c7e",
+  parchmentTan = "#d9cdb5",
+  inkBlack = "#1c1810",
+}
