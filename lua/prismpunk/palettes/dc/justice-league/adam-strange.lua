2f828900d68b1fe6851bf3f8d804550459c047d6
mavantgarderc <manihabibinava@gmail.com>
2025-11-24 03:34:55 +0330

    colorscheme(dc): justice-league-{adam-strange/black-canary/firestorm}

---
+++ lua/prismpunk/palettes/dc/justice-league/adam-strange.lua
@@ -0,0 +1,50 @@
+return {
+  bg_darkest = "#050508",
+  bg_darker = "#0a0a0e",
+  bg_dark = "#0f0f14",
+  bg_mid = "#16161d",
+  bg_light = "#2d2d35",
+  bg_lighter = "#44444d",
+  bg_lightest = "#5a5a64",
+
+  fg_lightest = "#f0f0f5",
+  fg_light = "#d8d8e0",
+  fg_mid = "#b0b0c0",
+  fg_dark = "#808090",
+
+  bg_alt1 = "#0d0d12",
+  bg_alt2 = "#070709",
+  bg_alt3 = "#12121a",
+  bg_alt4 = "#08080b",
+
+  championRed = "#ff0000",
+  rannDefender = "#ff1744",
+  heroicCrimson = "#e63946",
+  suitScarlet = "#dc143c",
+  alphaRed = "#c41e3a",
+
+  zetaBeam = "#00ffff",
+  teleportCyan = "#00e5ff",
+  quantumBlue = "#00bfff",
+  warpTeal = "#1e90ff",
+  dimensionalBlue = "#4169e1",
+
+  rayGunGold = "#ffd700",
+  blasterBeam = "#ffb300",
+  weaponChrome = "#c0c0c0",
+  finSilver = "#d3d3d3",
+
+  rannianCity = "#ff1493",
+  alannaLove = "#ff69b4",
+  princessPink = "#ffb6c1",
+  sardathPink = "#da70d6",
+
+  alienSky = "#9d4edd",
+  threeRedMoons = "#dc143c",
+  strangeWorld = "#8b00ff",
+  cosmicVoid = "#4b0082",
+
+  archaeologist = "#d2b48c",
+  earthBorn = "#8b7355",
+  stranded = "#b8860b",
+}
