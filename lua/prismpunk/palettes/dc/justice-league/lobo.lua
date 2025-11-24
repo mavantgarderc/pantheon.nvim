207df5259161563b03352b90783b30813c55fb93
mavantgarderc <manihabibinava@gmail.com>
2025-11-24 09:16:27 +0330

    colorscheme(dc): justice-league-lobo

---
+++ lua/prismpunk/palettes/dc/justice-league/lobo.lua
@@ -0,0 +1,30 @@
+return {
+
+  space_void = "#0b0014",
+  deep_czarnia = "#120022",
+  nebula_depth = "#1a0033",
+  exhaust_core = "#220044",
+
+  mainman_red = "#ff0033",
+  frag_red = "#ff1a44",
+  blood_glow = "#ff3355",
+  hook_crimson = "#ff5566",
+  cigar_cherry = "#ff7788",
+  arterial_spray = "#ff99aa",
+
+  chrome_steel = "#e0e0e0",
+  gunmetal = "#bbbbbb",
+  chain_silver = "#9999aa",
+  skull_white = "#ffffff",
+
+  cigar_ash = "#ffcc99",
+  beer_foam = "#ffffcc",
+
+  plasma_orange = "#ff6600",
+  muzzle_flash = "#ff8844",
+  ozone_blue = "#4488ff",
+
+  term_red = "#ff3366",
+  term_green = "#88ff88",
+  term_blue = "#6688ff",
+}
