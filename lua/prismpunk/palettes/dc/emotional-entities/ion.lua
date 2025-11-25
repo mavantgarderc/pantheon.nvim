74d12d60a3c717c6b8d0668710398e2b8ad71355
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:33:43 +0330

    colorsccheme(dc): emotional-entities-ion (will-power)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/ion.lua
@@ -0,0 +1,26 @@
+return {
+  voidGreen = "#0a1a0f",
+  abyssWill = "#0e2212",
+  emeraldCore = "#112f17",
+  deepResolve = "#00331a",
+
+  ionEmerald = "#00ff88",
+  willpowerGlow = "#00ff99",
+  absoluteGreen = "#00ffaa",
+  divineVerdant = "#00ffbb",
+  cosmicJade = "#00ffcc",
+
+  emeraldVein = "#00ff55",
+  unbreakable = "#00cc77",
+  victoryGold = "#aaff00",
+  sovereignFlame = "#88ff00",
+
+  radiantWill = "#88ffdd",
+  pureResolve = "#aaffdd",
+  godlightGreen = "#ccffee",
+  whiteWill = "#ffffff",
+
+  term_red = "#ff3366",
+  term_blue = "#0088ff",
+  term_magenta = "#ff00ff",
+}
