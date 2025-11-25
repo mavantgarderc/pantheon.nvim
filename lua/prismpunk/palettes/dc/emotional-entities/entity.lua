8ac1be28066cb421de90c0ba247a0aac9bb0b227
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:38:41 +0330

    colorscheme(dc): emotional-entities-entity (LIFE)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/entity.lua
@@ -0,0 +1,25 @@
+return {
+  earth_void = "#0a0e17",
+  biosphere_black = "#0e1421",
+  tectonic_depth = "#141d2e",
+  mantle_core = "#1a2539",
+
+  entity_white = "#ffffff",
+  life_radiance = "#f5fffa",
+  biosphere_glow = "#e8fff5",
+  verdant_light = "#d8ffed",
+  living_aura = "#c8ffe5",
+
+  ocean_blue = "#88ccff",
+  forest_green = "#88ffaa",
+  sky_cyan = "#88ffee",
+  sunrise_yellow = "#ffee88",
+  magma_orange = "#ffaa88",
+  coral_pink = "#ff88cc",
+
+  lifeless_red = "#ff3366",
+
+  term_red = "#ff6699",
+  term_green = "#88ffaa",
+  term_blue = "#88ccff",
+}
