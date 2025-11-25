25b951ee4b5c167caa3a4d84582575655a5cd826
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:37:41 +0330

    colorsccheme(dc): emotional-entities-proselyte (compassion)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/proselyte.lua
@@ -0,0 +1,24 @@
+return {
+
+  compassion_void = "#0d0033",
+  mercy_black = "#110044",
+  indigo_depth = "#1a0066",
+  empathy_core = "#220088",
+
+  proselyte_indigo = "#4488ff",
+  mercy_glow = "#5577ff",
+  empathy_light = "#6688ff",
+  gentle_aura = "#7799ff",
+  serene_bond = "#88bbff",
+
+  pale_compassion = "#99ccff",
+  tender_indigo = "#bbeeff",
+  soft_mercy = "#cceeff",
+  pure_compassion = "#ffffff",
+
+  suffering_red = "#ff4488",
+
+  term_red = "#ff6699",
+  term_green = "#88ffaa",
+  term_blue = "#6688ff",
+}
