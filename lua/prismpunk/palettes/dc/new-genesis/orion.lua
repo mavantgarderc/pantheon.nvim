87d80da9bef0a2da296cbd663a948273456d55f4
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 01:32:51 +0330

    colorscheme(dc): new-genesis-{orion/takion}

---
+++ lua/prismpunk/palettes/dc/new-genesis/orion.lua
@@ -0,0 +1,45 @@
+return {
+
+  apokolips_void = "#0c0018",
+  firepit_depth = "#18002a",
+  rage_shadow = "#260044",
+  war_core = "#380066",
+
+  warrior_skin = "#552b66",
+  dog_of_war = "#773388",
+  astro_violet = "#aa44cc",
+  orion_royal = "#cc66ff",
+
+  darkseid_red = "#ff0033",
+  rage_fire = "#ff2244",
+  war_crimson = "#ff4455",
+  astro_rage = "#ff6677",
+
+  new_genesis_gold = "#ffaa00",
+  honor_glow = "#ffcc22",
+  astro_gold = "#ffee44",
+  victory_sun = "#ffff66",
+
+  motherbox_fury = "#00ccff",
+  astro_cyan = "#22ddff",
+  rage_pulse = "#44eeff",
+
+  term_red = "#ff0033",
+  term_green = "#00ff88",
+  term_blue = "#00ccff",
+  term_yellow = "#ffaa00",
+  term_cyan = "#44eeff",
+
+  term_red_bright = "#ff3366",
+  term_green_bright = "#88ffaa",
+  term_yellow_bright = "#ffdd44",
+  term_blue_bright = "#88eeff",
+  term_magenta_bright = "#ff88ff",
+  term_cyan_bright = "#88ffff",
+
+  blood_trail = "#ff0011",
+  battle_scar = "#cc0022",
+  final_rage = "#ff4488",
+  astro_explosion = "#ffff00",
+  unbreakable_will = "#ff8800",
+}
---
+++ lua/prismpunk/palettes/dc/new-genesis/takion.lua
@@ -0,0 +1,47 @@
+return {
+
+  source_void = "#0a001a",
+  cosmic_depth = "#140032",
+  eternity_shadow = "#1e004d",
+  infinity_core = "#28006b",
+
+  joshua_skin = "#443388",
+  conduit_flesh = "#5e4aaa",
+  source_violet = "#8877dd",
+  takion_royal = "#a090ff",
+
+  source_light = "#ffffff",
+  source_glow = "#ffffee",
+  source_flare = "#ffffcc",
+  source_radiance = "#ffffaa",
+
+  cosmic_cyan = "#00ffff",
+  cosmic_teal = "#00ddee",
+  cosmic_aqua = "#00bbff",
+  cosmic_blue = "#0088ff",
+  cosmic_indigo = "#4466ff",
+
+  chronos_purple = "#cc00ff",
+  chronos_magenta = "#ee22ff",
+  chronos_pink = "#ff44ee",
+  chronos_lavender = "#ff88ff",
+
+  creation_gold = "#ffdd00",
+  destruction_red = "#ff0044",
+  rebirth_orange = "#ff6600",
+  equilibrium_green = "#00ff99",
+
+  term_red = "#ff0044",
+  term_green = "#00ff99",
+  term_blue = "#0088ff",
+  term_yellow = "#ffdd00",
+  term_cyan = "#00ffff",
+  term_magenta = "#ee22ff",
+
+  term_red_bright = "#ff4488",
+  term_green_bright = "#88ffcc",
+  term_yellow_bright = "#ffee66",
+  term_blue_bright = "#88bbff",
+  term_magenta_bright = "#ff88ff",
+  term_cyan_bright = "#88ffff",
+}
