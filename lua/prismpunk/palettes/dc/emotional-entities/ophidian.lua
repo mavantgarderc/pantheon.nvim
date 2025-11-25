14162dbd27e6ca1352b9c1634eefe1c677066eaa
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:34:15 +0330

    colorsccheme(dc): emotional-entities-ophidian (avarice)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/ophidian.lua
@@ -0,0 +1,23 @@
+return {
+  greedVoid = "#1a0d00",
+  avariceBlack = "#221200",
+  hungerDepth = "#331a00",
+  covetCore = "#442200",
+
+  ophidianOrange = "#ff8800",
+  greedFlame = "#ffaa00",
+  possessGold = "#ffcc00",
+  mineGlow = "#ffdd33",
+  eternalHunger = "#ffee66",
+
+  paleGreed = "#ffdd88",
+  covetBone = "#ffeeaa",
+  goldenFlesh = "#ffffcc",
+  pureWant = "#ffffff",
+
+  bloodGreed = "#ff0044",
+
+  term_red = "#ff4466",
+  term_green = "#44ff88",
+  term_blue = "#44aaff",
+}
