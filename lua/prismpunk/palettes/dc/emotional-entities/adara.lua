21ec3a2bb410cd0a50608ac9078448fec1bcd623
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:33:14 +0330

    colorsccheme(dc): emotional-entities-adara (hope)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/adara.lua
@@ -0,0 +1,28 @@
+return {
+  voidWhite = "#0f0f1a",
+  abyssBlue = "#131325",
+  deepHope = "#1a1a33",
+  cosmicBlue = "#1f1f44",
+
+  hopeCore = "#00eeff",
+  skyfireCyan = "#00ddff",
+  divineAzure = "#00ccff",
+  celestialGlow = "#00bbff",
+  starbirthBlue = "#00aaff",
+
+  angelFeather = "#ccffff",
+  wingsOfDawn = "#e6f5ff",
+  hopeRadiance = "#f0f8ff",
+  pureLight = "#ffffff",
+  etherealWhite = "#f8faff",
+
+  sapphireTear = "#4488ff",
+  miracleFlame = "#00ffcc",
+  auroraVein = "#00ffaa",
+  brokenChains = "#ffaa00",
+  goldenPromise = "#ffdd00",
+
+  term_red = "#ff3366",
+  term_blue = "#0088ff",
+  term_magenta = "#ff00ff",
+}
