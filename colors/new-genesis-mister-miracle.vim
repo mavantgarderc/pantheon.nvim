03588b25ab24536d6a22c0fea5a61633557906b2
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 01:34:35 +0330

    colorscheme(dc): new-genesis-mister-miracle

---
+++ colors/new-genesis-mister-miracle.vim
@@ -0,0 +1,21 @@
+if exists('g:colors_name')
+  hi clear
+endif
+
+if exists('syntax_on')
+  syntax reset
+endif
+
+let g:colors_name = "new-genesis/mister-miracle"
+
+lua << EOF
+local ok, prismpunk = pcall(require, "prismpunk")
+if ok and prismpunk.load_colorscheme then
+  local success, err = prismpunk.load_colorscheme("dc/new-genesis/mister-miracle")
+  if not success then
+    vim.notify("[prismpunk] Failed to load new-genesis/mister-miracle: " .. tostring(err), vim.log.levels.ERROR)
+  end
+else
+  vim.notify("[prismpunk] Plugin not loaded", vim.log.levels.ERROR)
+end
+EOF
---
+++ lua/prismpunk/palettes/dc/new-genesis/mister-miracle.lua
@@ -0,0 +1,50 @@
+return {
+
+  apokolips_void = "#0b0017",
+  orphan_depth = "#15002b",
+  granny_shadow = "#200048",
+  escape_core = "#2b0066",
+
+  free_skin = "#443377",
+  miracle_flesh = "#5d4a99",
+  escape_artist = "#8866cc",
+  scott_royal = "#aa88ff",
+
+  miracle_yellow = "#ffee00",
+  suit_green = "#00ff41",
+  cape_red = "#ff0033",
+
+  yellow_supernova = "#ffff3d",
+  green_hyperglow = "#66ff88",
+  red_heartfire = "#ff3366",
+  mask_dazzle = "#ffffff",
+
+  motherbox_turq = "#00ffff",
+  ping_ultra = "#33ffff",
+  life_equation = "#66ffff",
+  aero_discs = "#99ffff",
+  miracle_light = "#ccffff",
+
+  freedom_comet = "#ff00ff",
+  never_caught = "#00ffaa",
+  final_flash = "#ffff00",
+  scott_smile = "#ffff77",
+
+  term_red = "#ff0033",
+  term_green = "#00ff41",
+  term_yellow = "#ffee00",
+  term_cyan = "#00ffff",
+
+  unbreakable_joy = "#ffaa00",
+  love_wins = "#ff0066",
+  super_escape = "#00ff00",
+  hope_explosion = "#ffff55",
+
+  term_red_bright = "#ff3366",
+  term_green_bright = "#66ff88",
+  term_yellow_bright = "#ffff55",
+  term_blue_bright = "#88ffff",
+  term_magenta_bright = "#ff88ff",
+  term_cyan_bright = "#ccffff",
+  term_black_bright = "#331144",
+}
