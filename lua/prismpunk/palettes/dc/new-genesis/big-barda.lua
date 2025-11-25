8e2b189701643982cb2d9c72d6d43ce82c1bcfd3
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 01:31:05 +0330

    colorscheme(dc): new-genesis-{big-barda/highfather}

---
+++ lua/prismpunk/palettes/dc/new-genesis/big-barda.lua
@@ -0,0 +1,44 @@
+return {
+
+  fury_void = "#0a001a",
+  apokolips_scar = "#120030",
+  broken_chain = "#1a004d",
+  freedom_depth = "#240066",
+
+  warrior_steel = "#2a3377",
+  fury_flesh = "#3d4d99",
+  honor_skin = "#5566bb",
+  barda_blue = "#7088ff",
+
+  barda_royal = "#4488ff",
+  freedom_blue = "#5599ff",
+  mega_rod_blue = "#66aaff",
+  justice_glow = "#88bbff",
+  righteous_light = "#aaddff",
+
+  honor_gold = "#ffcc00",
+  miracle_gold = "#ffdd22",
+  love_ember = "#ffee44",
+  victory_gold = "#ffff66",
+  eternal_bond = "#ffffaa",
+
+  new_genesis_white = "#ffffff",
+  hope_core = "#eeeeff",
+  freedom_dawn = "#ddddff",
+
+  barda_red = "#ff3366",
+  fury_crimson = "#ff5577",
+  love_pulse = "#ff7799",
+
+  term_red = "#ff4488",
+  term_green = "#aaff00",
+  term_blue = "#4488ff",
+  term_yellow = "#ffdd44",
+  term_cyan = "#88bbff",
+
+  mega_rod_core = "#0033aa",
+  fury_shadow = "#002266",
+  honor_pulse = "#0044cc",
+  battle_scars = "#0066ff",
+  unbreakable = "#0088ff",
+}
---
+++ lua/prismpunk/palettes/dc/new-genesis/highfather.lua
@@ -0,0 +1,42 @@
+return {
+
+  source_void = "#05051a",
+  genesis_depth = "#0a0a30",
+  celestial_shadow = "#0f0f4d",
+  eternal_core = "#141466",
+
+  prophet_skin = "#2a3377",
+  shepherd_flesh = "#3d4d99",
+  inheritor_blue = "#5566cc",
+  izaya_royal = "#7088ff",
+
+  highfather_white = "#ffffff",
+  source_light = "#eeeeff",
+  genesis_glow = "#ddddff",
+  divine_radiance = "#ccccff",
+  hope_dawn = "#bbbbff",
+
+  source_gold = "#ffdd00",
+  wisdom_gold = "#ffee22",
+  eternal_truth = "#ffff44",
+  shepherd_staff = "#ffff88",
+  miracle_light = "#ffffbb",
+
+  source_blue = "#4488ff",
+  universal_truth = "#5599ff",
+  infinity_glow = "#66aaff",
+  cosmic_peace = "#88bbff",
+  heavenly_azure = "#aaddff",
+
+  term_red = "#ff4488",
+  term_green = "#aaff88",
+  term_blue = "#4488ff",
+  term_yellow = "#ffee88",
+  term_cyan = "#88bbff",
+
+  staff_core = "#0033cc",
+  prophecy_pulse = "#0044ee",
+  source_fracture = "#0055ff",
+  divine_mercy = "#3377ff",
+  final_light = "#5599ff",
+}
