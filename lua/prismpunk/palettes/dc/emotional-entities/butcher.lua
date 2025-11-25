ae444961824f9d56992591d5c57a80bb74aefb77
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:33:30 +0330

    colorsccheme(dc): emotional-entities-butcher (rage)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/butcher.lua
@@ -0,0 +1,26 @@
+return {
+  goreBlack = "#0c0709",
+  bloodVoid = "#11080c",
+  rageDepth = "#180a10",
+  slaughterCore = "#1f0b14",
+
+  butcherRed = "#ff0044",
+  arterialFlame = "#ff2255",
+  carnageRed = "#ff3366",
+  rageFire = "#ff5566",
+  hellGlow = "#ff7788",
+
+  flayedFlesh = "#ff9999",
+  boneSnap = "#ffbbbb",
+  skullWhite = "#ffdddd",
+  pureBone = "#ffffff",
+
+  demonAsh = "#cc2222",
+  moltenVein = "#ff0000",
+  boilingBlood = "#ff4400",
+  wrathGold = "#ff8800",
+
+  term_green = "#00ff00",
+  term_blue = "#0088ff",
+  term_cyan = "#00ffff",
+}
