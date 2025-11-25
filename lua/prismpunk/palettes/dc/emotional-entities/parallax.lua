664269e78810e216a5010ed1d91b541e877f2648
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:34:28 +0330

    colorsccheme(dc): emotional-entities-parallalx (fear)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/parallax.lua
@@ -0,0 +1,23 @@
+return {
+  fearVoid = "#1a1400",
+  terrorBlack = "#221c00",
+  dreadDepth = "#332800",
+  panicCore = "#443800",
+
+  parallaxYellow = "#ffff00",
+  fearBattery = "#ffdd00",
+  panicGold = "#ffbb00",
+  screamYellow = "#ffaa00",
+  nightmareGlow = "#ff8800",
+
+  coldSweat = "#ffff88",
+  tremblingBone = "#ffffaa",
+  ghostPale = "#ffffcc",
+  pureTerror = "#ffffff",
+
+  bloodFear = "#ff2244",
+
+  term_red = "#ff4466",
+  term_green = "#44ff88",
+  term_blue = "#44aaff",
+}
