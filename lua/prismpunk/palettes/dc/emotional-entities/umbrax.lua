2c6339f35195d070a56e5e6ee69dbaf49ca31eeb
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:39:13 +0330

    colorscheme(dc): emotional-entities-umbrax (INVISIBLE EMOTIONAL SPECTRUM)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/umbrax.lua
@@ -0,0 +1,24 @@
+return {
+
+  death_void = "#000000",
+  umbra_core = "#0a0a0f",
+  eclipse_depth = "#111118",
+  oblivion_black = "#16161e",
+
+  umbrax_uv = "#8b00ff",
+  necrotic_pulse = "#9f33ff",
+  void_flare = "#b366ff",
+  grave_glow = "#cc99ff",
+  spectral_shine = "#e6ccff",
+
+  corpse_pale = "#d0d0e6",
+  ash_gray = "#bbbbe0",
+  bone_dust = "#a8a8d8",
+  pure_death = "#ffffff",
+
+  dead_blood = "#660033",
+
+  term_red = "#ff3366",
+  term_green = "#66ff88",
+  term_blue = "#8866ff",
+}
