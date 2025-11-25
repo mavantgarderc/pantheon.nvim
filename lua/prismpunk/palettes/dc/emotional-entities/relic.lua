dd3e7e72df27702e6d5a46e57e9dab4517dc73f1
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:38:08 +0330

    colorsccheme(dc): emotional-entities-relic (light promordial)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/relic.lua
@@ -0,0 +1,28 @@
+return {
+
+  life_void = "#0f1117",
+  creation_black = "#171925",
+  genesis_depth = "#222533",
+  origin_core = "#2d3344",
+
+  relic_white = "#ffffff",
+  divine_radiance = "#f0f4ff",
+  eternal_brill = "#e0e8ff",
+  pure_lumen = "#d0dcff",
+  holy_glow = "#c0d0ff",
+
+  prism_violet = "#e0d0ff",
+  prism_indigo = "#d0dcff",
+  prism_blue = "#c0e8ff",
+  prism_cyan = "#c0f0ff",
+  prism_green = "#c8ffd8",
+  prism_yellow = "#ffffc0",
+  prism_orange = "#ffe8c0",
+  prism_red = "#ffd0d0",
+
+  fallen_shadow = "#ff3366",
+
+  term_red = "#ff6699",
+  term_green = "#88ffcc",
+  term_blue = "#88ccff",
+}
