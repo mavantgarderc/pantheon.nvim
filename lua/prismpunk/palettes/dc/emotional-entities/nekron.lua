045485319f385ab98950bde76d678e5a40c71b42
mavantgarderc <manihabibinava@gmail.com>
2025-11-25 07:33:57 +0330

    colorsccheme(dc): emotional-entities-nekron (death)

---
+++ lua/prismpunk/palettes/dc/emotional-entities/nekron.lua
@@ -0,0 +1,24 @@
+return {
+
+  blackVoid = "#000000",
+  deepAbyss = "#0d0f14",
+  graveDepth = "#161822",
+  tombStone = "#1e212b",
+
+  boneWhite = "#c8d0e0",
+  paleCorpse = "#a8b8cc",
+  ashGrey = "#8895b0",
+  coldFlesh = "#687588",
+
+  deathGreen = "#00ff88",
+  necroticEmerald = "#00dd77",
+  reaperJade = "#00bb66",
+  soulSiphon = "#00995a",
+  blackBattery = "#00774a",
+
+  finalRed = "#cc2244",
+
+  term_red = "#ff4466",
+  term_blue = "#44aaff",
+  term_magenta = "#ff66ff",
+}
