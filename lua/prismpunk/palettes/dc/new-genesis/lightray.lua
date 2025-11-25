6622622fd82b1c556c67fa8351045621f998a2e8
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 01:32:17 +0330

    colorscheme(dc): new-genesis-{lightray/metron}

---
+++ lua/prismpunk/palettes/dc/new-genesis/lightray.lua
@@ -0,0 +1,42 @@
+return {
+
+  genesis_void = "#000822",
+  dawn_depth = "#001144",
+  prelight_shadow = "#002266",
+  horizon_core = "#003388",
+
+  solor_skin = "#3355aa",
+  shining_flesh = "#4477cc",
+  radiant_blue = "#5599ff",
+  lightray_core = "#66bbff",
+
+  lightray_white = "#ffffff",
+  solar_flare = "#ffffee",
+  shining_radiance = "#ffffdd",
+  hope_brilliance = "#ffffcc",
+  eternal_sun = "#ffffbb",
+
+  photon_cyan = "#00ffff",
+  lightray_cyan = "#22ffee",
+  speed_force = "#44ffdd",
+  aurora_glow = "#66ffcc",
+  prism_light = "#88ffbb",
+
+  sunrise_gold = "#ffdd00",
+  solor_gold = "#ffee22",
+  joy_dawn = "#ffff44",
+  radiant_smile = "#ffff77",
+  victory_sun = "#ffffaa",
+
+  term_red = "#ff4466",
+  term_green = "#00ffaa",
+  term_blue = "#00ffff",
+  term_yellow = "#ffee00",
+  term_cyan = "#44ffdd",
+
+  light_trail = "#0088ff",
+  beam_core = "#00aaff",
+  flash_pulse = "#00ccff",
+  nova_burst = "#00eeff",
+  final_shine = "#11ffff",
+}
---
+++ lua/prismpunk/palettes/dc/new-genesis/metron.lua
@@ -0,0 +1,42 @@
+return {
+
+  metron_void = "#0a0014",
+  timeless_depth = "#120028",
+  chronal_shadow = "#1a0040",
+  eternity_core = "#240066",
+
+  observer_skin = "#332b55",
+  chronal_flesh = "#4d4477",
+  mobius_blue = "#665599",
+  metron_purple = "#7f6bb3",
+
+  source_knowledge = "#aaddff",
+  chronal_energy = "#88bbff",
+  mobius_current = "#6699ff",
+  timeless_glow = "#4488ff",
+  absolute_zero = "#2266ff",
+
+  mobius_silver = "#cccccc",
+  chronal_steel = "#bbbbdd",
+  observer_white = "#eeeeff",
+  void_light = "#ddddff",
+  sterile_radiance = "#ccccff",
+
+  anti_life_violet = "#aa00ff",
+  paradox_purple = "#bb22ff",
+  eternity_pulse = "#cc44ff",
+  singularity_glow = "#dd88ff",
+  final_truth = "#eebbff",
+
+  term_red = "#ff4488",
+  term_green = "#88ffaa",
+  term_blue = "#4488ff",
+  term_yellow = "#ffdd88",
+  term_cyan = "#88ccff",
+
+  time_fracture = "#110033",
+  paradox_core = "#220066",
+  mobius_coil = "#330099",
+  chronal_rift = "#4400cc",
+  final_moment = "#5500ff",
+}
