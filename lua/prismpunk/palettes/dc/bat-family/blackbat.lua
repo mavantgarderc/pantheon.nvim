e601e2d91fe34b814c44fe665895ca93e2dca930
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 01:26:39 +0330

    colorscheme(dc): bat-family-{james-gordon/blackbat}

---
+++ lua/prismpunk/palettes/dc/bat-family/blackbat.lua
@@ -0,0 +1,46 @@
+return {
+
+  void_black = "#0a0a0f",
+  alley_black = "#0f0f16",
+  orphan_shadow = "#171722",
+  lethal_core = "#21212e",
+
+  ghost_skin = "#4b4455",
+  bruised_flesh = "#5d5570",
+  silent_purple = "#7a6999",
+  orphan_royal = "#9988cc",
+
+  arterial_red = "#cc0033",
+  fresh_blood = "#ee1144",
+  dried_blood = "#aa2233",
+  wound_glow = "#ff4466",
+
+  ghost_white = "#e8e8ff",
+  moonlit_blade = "#ccccff",
+  silent_gaze = "#aaaaff",
+
+  batcloth_gray = "#2d2d44",
+  midnight_cape = "#1a1a2e",
+  stealth_indigo = "#3a3377",
+
+  fresh_bruise = "#773399",
+  old_bruise = "#554477",
+  healing_bruise = "#665588",
+
+  hidden_heart = "#ff6699",
+  david_cain_red = "#880022",
+
+  term_red = "#cc0033",
+  term_green = "#00aa88",
+  term_blue = "#6666ff",
+  term_yellow = "#ffcc99",
+  term_cyan = "#88cccc",
+  term_magenta = "#cc66cc",
+
+  term_red_bright = "#ff3366",
+  term_green_bright = "#55ccaa",
+  term_yellow_bright = "#ffddaa",
+  term_blue_bright = "#9999ff",
+  term_magenta_bright = "#ee88ee",
+  term_cyan_bright = "#aadddd",
+}
---
+++ lua/prismpunk/palettes/dc/bat-family/james-gordon.lua
@@ -0,0 +1,46 @@
+return {
+
+  gotham_night = "#0b0e14",
+  alley_depth = "#13161f",
+  rain_shadow = "#1c222b",
+  midnight_core = "#262d38",
+
+  gordon_skin = "#5d5044",
+  trench_coat = "#6d624f",
+  honest_gray = "#8c8070",
+  veteran_brown = "#a89a80",
+
+  batsignal_yellow = "#ffd42a",
+  signal_glow = "#ffe97b",
+  beacon_light = "#ffffc0",
+  hope_white = "#ffffff",
+
+  beacon_red = "#ff0033",
+  siren_red = "#ff3355",
+  emergency_red = "#ff5566",
+  gotham_pd_blue = "#0066cc",
+  patrol_blue = "#3388ff",
+  nightshift_cyan = "#00aadd",
+
+  cigarette_ember = "#ff4400",
+  coffee_brown = "#8b4513",
+  late_night = "#b5651d",
+
+  manila_folder = "#d4c9a8",
+  casefile_beige = "#e0d6b8",
+  red_pen = "#cc0000",
+
+  term_red = "#ff0033",
+  term_green = "#88aa00",
+  term_blue = "#0066cc",
+  term_yellow = "#ffd42a",
+  term_cyan = "#00aadd",
+  term_magenta = "#cc66aa",
+
+  term_red_bright = "#ff5588",
+  term_green_bright = "#aadd55",
+  term_yellow_bright = "#ffee88",
+  term_blue_bright = "#5599ff",
+  term_magenta_bright = "#ff88cc",
+  term_cyan_bright = "#88dddd",
+}
