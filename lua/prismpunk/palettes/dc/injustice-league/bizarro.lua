40b5fc6e69d16cdba06b9c128ce9219818bd71ee
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 05:43:36 +0330

    colorscheme(dc): fixed injustice-league-bizarro

--- lua/prismpunk/palettes/dc/injustice-league/bizarro.lua
+++ lua/prismpunk/palettes/dc/injustice-league/bizarro.lua
@@ -1,98 +1,32 @@
 return {
 
-  bg_darkest = "#0A1408",
-  bg_darker = "#0F1A0D",
-  bg_dark = "#152012",
-  bg_mid = "#1A2617",
-  bg_light = "#20301C",
-  bg_lighter = "#2A3A26",
-  bg_lightest = "#344430",
+  bg_darkest = "#041022",
+  bg_darker = "#081427",
+  bg_dark = "#111827",
+  bg_mid = "#182230",
+  bg_light = "#28333B",
+  bg_lighter = "#6B7780",
+  bg_lightest = "#B4C4CF",
 
-  fg_lightest = "#E6FFF0",
-  fg_light = "#C8F0D8",
-  fg_mid = "#A0D0B0",
-  fg_dark = "#78B088",
+  fg_lightest = "#6B7780",
+  fg_light = "#B4C4CF",
+  fg_mid = "#E8F4FF",
+  fg_dark = "#FFF7E6",
 
-  bg_alt1 = "#12180E",
-  bg_alt2 = "#0C1409",
-  bg_alt3 = "#17210F",
-  bg_alt4 = "#0E150A",
+  bg_alt1 = "#0C1523",
+  bg_alt2 = "#060F1D",
+  bg_alt3 = "#141D2D",
+  bg_alt4 = "#0E1828",
 
-  corruptedGreenCore = "#52CC52",
-  toxicYellowCore = "#CCFF33",
-  backwardsPurpleCore = "#9D4EDD",
-  brokenMagentaCore = "#E91E63",
-  oppositeOrangeCore = "#FF8C42",
-
-  corruptedGreen = "#52CC52",
-  corruptedGreenBright = "#7AFF7A",
-  corruptedGreenDeep = "#3DA03D",
-
-  toxicYellow = "#CCFF33",
-  toxicYellowBright = "#E0FF66",
-  toxicYellowCore = "#CCFF33",
-  toxicYellowDeep = "#B3E000",
-
-  backwardsPurple = "#9D4EDD",
-  backwardsPurpleBright = "#C77DFF",
-  backwardsPurpleCore = "#9D4EDD",
-  backwardsPurpleDeep = "#7B2CBF",
-
-  brokenMagenta = "#E91E63",
-  brokenMagentaBright = "#FF4081",
-  brokenMagentaCore = "#E91E63",
-  brokenMagentaDeep = "#C2185B",
-
-  oppositeOrange = "#FF8C42",
-  oppositeOrangeBright = "#FFAA66",
-  oppositeOrangeCore = "#FF8C42",
-  oppositeOrangeDeep = "#E67728",
-
-  decayGray = "#6B7A6B",
-  decayGrayLight = "#85A885",
-  decayGrayDark = "#546854",
-
-  invertedWhite = "#F0FFF5",
-  invertedWhiteCore = "#F0FFF5",
-  invertedWhiteBright = "#FAFFFD",
-
-  chaosLime = "#AAFF00",
-  chaosLimeBright = "#CCFF33",
-  chaosLimeCore = "#AAFF00",
-
-  confusionRed = "#FF3366",
-  confusionRedBright = "#FF5588",
-  confusionRedCore = "#FF3366",
-
-  currentGray = "#6B7A6B",
-  currentGrayLight = "#85A885",
-
-  backwardsSuccess = "#9D4EDD",
-  backwardsSuccessCore = "#9D4EDD",
-  backwardsSuccessBright = "#C77DFF",
-
-  wrongViolet = "#8B5CF6",
-  wrongVioletCore = "#8B5CF6",
-
-  voidBlack = "#0A1408",
-
-  successGreen = "#9D4EDD",
-  errorRed = "#52CC52",
-  warningAmber = "#CCFF33",
-  infoBlue = "#E91E63",
-
-  seaweedGreenCore = "#9D4EDD",
-  anglerYellowCore = "#CCFF33",
-  anemoneRedCore = "#52CC52",
-
-  coralPink = "#E91E63",
-  coralPinkBright = "#FF4081",
-  coralPinkCore = "#E91E63",
-  coralPinkDeep = "#C2185B",
-
-  pressureWhite = "#E6FFF0",
-  pressureWhiteCore = "#E6FFF0",
-  pressureWhiteBright = "#F0FFF5",
-
-  depthBlack = "#0A1408",
+  solarBlue = "#1E6FBF",
+  skyAzure = "#2F7FBF",
+  capeRed = "#D73A3A",
+  emblemGold = "#FFD35A",
+  justiceYellow = "#FFEA8A",
+  fortressSilver = "#D9E6F0",
+  kryptonCrimson = "#FF6666",
+  solarFlare = "#FFD28A",
+  hopeBlue = "#61AFEF",
+  steelResolve = "#7FA3B8",
+  azureBright = "#6FBFFF",
 }
