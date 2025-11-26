93a0385936f3513802bc0509449a4566153c708a
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 05:46:01 +0330

    colorscheme(dc): injustice-league-killercroc

---
+++ lua/prismpunk/palettes/dc/injustice-league/killercroc.lua
@@ -0,0 +1,49 @@
+return {
+
+  sewer_black = "#0a0c0b",
+  wet_stone = "#0f1412",
+  slime_depth = "#121918",
+  swamp_core = "#16201d",
+
+  croc_green = "#1a3322",
+  reptilian = "#2d4a33",
+  toxic_scale = "#3d6644",
+  swamp_olive = "#558866",
+
+  toxic_sludge = "#44aa22",
+  bile_green = "#66cc33",
+  venom_lime = "#88ff55",
+  acid_glow = "#aaff77",
+
+  fresh_blood = "#aa2211",
+  dried_blood = "#771100",
+  meat_red = "#cc3322",
+  gore_red = "#ff4433",
+
+  rust_dark = "#663322",
+  rust = "#884433",
+  chain_iron = "#aa6655",
+  sewer_rust = "#cc8866",
+
+  croc_yellow = "#ffdd66",
+  tooth_bone = "#ffeeaa",
+  predator_eye = "#ffff88",
+
+  sewer_teal = "#226655",
+  mold_cyan = "#33aa99",
+  stagnant_water = "#448877",
+
+  term_red = "#ff4433",
+  term_green = "#66cc33",
+  term_blue = "#448877",
+  term_yellow = "#ffdd66",
+  term_cyan = "#33aa99",
+  term_magenta = "#cc6677",
+
+  term_red_bright = "#ff7766",
+  term_green_bright = "#88ee55",
+  term_yellow_bright = "#ffee99",
+  term_blue_bright = "#66ccbb",
+  term_magenta_bright = "#ee88aa",
+  term_cyan_bright = "#55cccc",
+}
