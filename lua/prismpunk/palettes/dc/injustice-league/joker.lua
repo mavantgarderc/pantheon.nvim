84863d6617e0abadf28be593d5479d2e1c88238a
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 05:45:53 +0330

    colorscheme(dc): injustice-league-joker

---
+++ lua/prismpunk/palettes/dc/injustice-league/joker.lua
@@ -0,0 +1,48 @@
+return {
+
+  abyss_black = "#0b0011",
+  chemical_void = "#12001a",
+  madness_shadow = "#1a0026",
+  grin_core = "#250038",
+
+  dead_skin = "#c8c8d4",
+  sick_flesh = "#a8a8c0",
+  grin_purple = "#bb88ff",
+  psycho_royal = "#dd99ff",
+
+  acid_green = "#00ff44",
+  toxic_lime = "#44ff88",
+  chemical_burn = "#88ffaa",
+  joker_venom = "#aaffc3",
+
+  arterial_red = "#ff0033",
+  lipstick_red = "#ff2255",
+  murder_red = "#ff4466",
+  chaos_red = "#ff6688",
+
+  grin_yellow = "#ffff44",
+  fever_dream = "#ffff88",
+  broken_mind = "#ffffbb",
+
+  purple_suit = "#8800ff",
+  violet_chaos = "#aa33ff",
+  ultraviolet = "#cc66ff",
+
+  laughing_gas = "#ff00cc",
+  punchline_pink = "#ff44ff",
+  bat_break = "#ff88ff",
+
+  term_red = "#ff0033",
+  term_green = "#00ff44",
+  term_blue = "#8800ff",
+  term_yellow = "#ffff44",
+  term_cyan = "#00ffcc",
+  term_magenta = "#ff00cc",
+
+  term_red_bright = "#ff4488",
+  term_green_bright = "#88ffbb",
+  term_yellow_bright = "#ffff99",
+  term_blue_bright = "#bb88ff",
+  term_magenta_bright = "#ff88ff",
+  term_cyan_bright = "#88ffff",
+}
