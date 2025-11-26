db10476ccce376f02d3cc587a9e744eadea50dc1
mavantgarderc <manihabibinava@gmail.com>
2025-11-26 08:31:44 +0330

    fix(higlights): git highlight integration fixed

--- lua/prismpunk/core/highlights/git.lua
+++ lua/prismpunk/core/highlights/git.lua
@@ -11,12 +11,12 @@ function M.apply(c, _config)
   hl("DiffDelete", { fg = s.diff.delete, bg = s.ui.bg })
   hl("DiffText", { fg = s.diff.text, bg = s.ui.bg_dim })
 
-  hl("GitSignsAdd", { fg = s.git.add })
-  hl("GitSignsChange", { fg = s.git.change })
-  hl("GitSignsDelete", { fg = s.git.delete })
-  hl("GitSignsAddInline", { fg = s.git.add_inline or s.git.add, reverse = true })
-  hl("GitSignsChangeInline", { fg = s.git.change_inline or s.git.change, reverse = true })
-  hl("GitSignsDeleteInline", { fg = s.git.delete_inline or s.git.delete, reverse = true })
+  hl("GitSignsAdd", { fg = s.diff.add })
+  hl("GitSignsChange", { fg = s.diff.change })
+  hl("GitSignsDelete", { fg = s.diff.delete })
+  hl("GitSignsAddInline", { fg = s.diff.add_inline or s.diff.add, reverse = true })
+  hl("GitSignsChangeInline", { fg = s.diff.change_inline or s.diff.change, reverse = true })
+  hl("GitSignsDeleteInline", { fg = s.diff.delete_inline or s.diff.delete, reverse = true })
 end
 
 return M
