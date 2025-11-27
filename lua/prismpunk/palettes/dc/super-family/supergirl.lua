384f890741f122dc264b7ab278126eb0f34bc4a7
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:57:33 +0330

    colorscheme(dc): super-family
    - supergirl
    - superman-classic

---
+++ lua/prismpunk/palettes/dc/super-family/supergirl.lua
@@ -0,0 +1,33 @@
+return {
+  void_black = "#0c1020",
+  argo_night = "#0f152c",
+  kara_core = "#142040",
+  sky_glow = "#1a2a55",
+
+  kara_blue = "#00aaff",
+  supergirl_blue = "#00bbff",
+  sky_blue = "#00ccff",
+  hope_blue = "#66ddff",
+
+  kara_red = "#ff3366",
+  cape_red = "#ff4477",
+  shield_red = "#ff5588",
+  heart_red = "#ff77aa",
+
+  shield_gold = "#ffea00",
+  solar_gold = "#ffee44",
+  hope_gold = "#ffff88",
+
+  crystal_white = "#f8fcff",
+  argo_white = "#e6f5ff",
+  kara_white = "#ccedff",
+
+  term_blue = "#00bbff",
+  term_red = "#ff4477",
+  term_gold = "#ffee44",
+  term_white = "#ccedff",
+
+  term_blue_bright = "#66ddff",
+  term_red_bright = "#ff77aa",
+  term_gold_bright = "#ffff99",
+}
---
+++ lua/prismpunk/palettes/dc/super-family/superman-classic.lua
@@ -0,0 +1,34 @@
+return {
+
+  void_black = "#0a0e1a",
+  classic_void = "#0d1224",
+  dailyplanet_core = "#121a30",
+  metropolis_glow = "#182540",
+
+  classic_blue = "#0066ff",
+  superman_blue = "#0077ff",
+  sky_blue = "#0088ff",
+  hero_blue = "#4499ff",
+
+  classic_red = "#ff1111",
+  cape_red = "#ff2222",
+  shield_red = "#ff3333",
+  metropolis_red = "#ff5555",
+
+  shield_yellow = "#ffdd00",
+  solar_yellow = "#ffee00",
+  hope_yellow = "#ffff22",
+  kryptonian_sun = "#ffff66",
+
+  newsprint_white = "#f0f4ff",
+  dailyplanet_cream = "#e0e8ff",
+
+  term_blue = "#0077ff",
+  term_red = "#ff2222",
+  term_yellow = "#ffee00",
+  term_cream = "#e0e8ff",
+
+  term_blue_bright = "#4499ff",
+  term_red_bright = "#ff6666",
+  term_yellow_bright = "#ffff88",
+}
