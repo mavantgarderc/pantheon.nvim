38cb9e1902325e508da1cc5a23fd08f7426f60eb
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 04:17:42 +0330

    colorscheme(dc): injustice-league
    - captain-cold
    - cheetah
    - giganta
    - gorilla grod

---
+++ lua/prismpunk/palettes/dc/injustice-league/giganta.lua
@@ -0,0 +1,46 @@
+return {
+
+  void_black = "#0c080a",
+  titan_void = "#110d14",
+  colossal_core = "#181222",
+  atomic_glow = "#221833",
+
+  titan_orange = "#ff6611",
+  colossal_orange = "#ff8833",
+  magma_skin = "#ffaa55",
+  eruption_glow = "#ffcc88",
+  atomic_heat = "#ff4400",
+
+  atomic_red = "#ff1122",
+  inferno_red = "#ff3333",
+  titan_rage = "#ff5555",
+  volcano_hair = "#ff7766",
+
+  titan_purple = "#aa22ff",
+  colossal_violet = "#bb44ff",
+  strength_pulse = "#cc66ff",
+  royal_violence = "#dd88ff",
+
+  titan_blue = "#2277ff",
+  arctic_fury = "#4499ff",
+  glacial_gaze = "#66bbff",
+
+  rubble_grey = "#666677",
+  crushed_stone = "#888899",
+  earthquake_dust = "#444455",
+
+  term_red = "#ff3333",
+  term_orange = "#ff8833",
+  term_purple = "#bb44ff",
+  term_blue = "#4499ff",
+  term_green = "#44cc88",
+  term_yellow = "#ffdd66",
+  term_cyan = "#66ccff",
+
+  term_red_bright = "#ff6666",
+  term_orange_bright = "#ffaa77",
+  term_purple_bright = "#dd77ff",
+  term_blue_bright = "#77bbff",
+  term_green_bright = "#66eeaa",
+  term_yellow_bright = "#ffee99",
+}
