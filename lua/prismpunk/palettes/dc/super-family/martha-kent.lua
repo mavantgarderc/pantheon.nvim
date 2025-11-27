27c81943a3ec41f3fa166ebe1b556d4c0496baba
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:57:13 +0330

    colorscheme(dc): super-family
    - martha kent
    - steel

---
+++ lua/prismpunk/palettes/dc/super-family/martha-kent.lua
@@ -0,0 +1,35 @@
+return {
+
+  void_black = "#0a0c10",
+  smallville_night = "#0d1118",
+  barn_core = "#121924",
+  porch_glow = "#182030",
+
+  denim_blue = "#335577",
+  martha_blue = "#446688",
+  kansas_sky = "#5588aa",
+  hope_blue = "#77aadd",
+
+  barn_red = "#aa3322",
+  flannel_red = "#bb4433",
+  heart_red = "#cc6655",
+  sunrise_red = "#ee8877",
+
+  wheat_gold = "#ddbb88",
+  martha_gold = "#eecc99",
+  smallville_sun = "#ffddaa",
+  hope_gold = "#ffeecc",
+
+  cream_white = "#fffff0",
+  farmhouse_white = "#f8f8f0",
+  ma_white = "#eeeee4",
+
+  term_red = "#bb4433",
+  term_blue = "#5588aa",
+  term_gold = "#eecc99",
+  term_cream = "#f8f8f0",
+
+  term_red_bright = "#ee8877",
+  term_blue_bright = "#77aadd",
+  term_gold_bright = "#ffeecc",
+}
---
+++ lua/prismpunk/palettes/dc/super-family/steel.lua
@@ -0,0 +1,29 @@
+return {
+
+  void_black = "#0a0c10",
+  steel_night = "#0d1116",
+  forge_core = "#12181f",
+  hammer_glow = "#18202a",
+
+  gunmetal = "#1e242e",
+  riveted_steel = "#283040",
+  battleship_gray = "#354050",
+  polished_steel = "#506070",
+
+  steel_red = "#cc1122",
+  hammer_red = "#dd2233",
+  kinetic_red = "#ee3344",
+  metropolis_red = "#ff5566",
+
+  star_white = "#eef2ff",
+  flag_blue = "#002244",
+  liberty_blue = "#003366",
+  steel_sky = "#335588",
+
+  term_red = "#dd2233",
+  term_blue = "#335588",
+  term_white = "#eef2ff",
+
+  term_red_bright = "#ff5566",
+  term_blue_bright = "#5588cc",
+}
