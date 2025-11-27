82c2f2eec6325c33d86eb369f61ebfdd88e3abc2
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:56:44 +0330

    colorscheme(dc): super-family
    - lois lane
    - lucy lane

---
+++ lua/prismpunk/palettes/dc/super-family/lois-lane.lua
@@ -0,0 +1,34 @@
+return {
+
+  void_black = "#0c0a12",
+  planet_night = "#0f0d18",
+  ink_core = "#141124",
+  deadline_glow = "#1a1628",
+
+  navy_trench = "#112244",
+  midnight_navy = "#1a2a55",
+  daily_planet_blue = "#223366",
+  reporter_navy = "#334477",
+
+  lois_red = "#cc1133",
+  breaking_red = "#dd2244",
+  headline_crimson = "#ee3355",
+  scoop_rage = "#ff5577",
+
+  lois_violet = "#aa22ff",
+  truth_purple = "#bb33ff",
+  justice_purple = "#cc55ff",
+  expose_purple = "#dd77ff",
+
+  press_white = "#eeffff",
+  page_cream = "#ddeeff",
+  ink_paper = "#cce0ff",
+
+  term_red = "#dd2244",
+  term_violet = "#bb33ff",
+  term_navy = "#223366",
+  term_cream = "#ddeeff",
+
+  term_red_bright = "#ff5577",
+  term_violet_bright = "#dd77ff",
+}
---
+++ lua/prismpunk/palettes/dc/super-family/lucy-lane.lua
@@ -0,0 +1,27 @@
+return {
+
+  void_black = "#0a080c",
+  reactor_void = "#0c0910",
+  superwoman_core = "#120d18",
+  redfield_glow = "#180e20",
+
+  lane_red = "#cc1122",
+  superwoman_red = "#dd2233",
+  reactor_red = "#ee3344",
+  kryptonian_red = "#ff5566",
+
+  shield_black = "#111111",
+  tactical_gray = "#333344",
+  lane_gray = "#445566",
+  command_blue = "#557799",
+
+  energy_white = "#ffdddd",
+  reactor_white = "#ffeeee",
+
+  term_red = "#dd2233",
+  term_blue = "#557799",
+  term_white = "#ffeeee",
+
+  term_red_bright = "#ff5566",
+  term_blue_bright = "#88aadd",
+}
