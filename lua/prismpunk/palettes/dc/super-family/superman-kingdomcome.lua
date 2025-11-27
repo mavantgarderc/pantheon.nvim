0d944b4ccf1cc30348a3d0c4573ed8fc1804ff45
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:57:51 +0330

    colorscheme(dc): super-family
    - kingdomcome
    - modern

---
+++ lua/prismpunk/palettes/dc/super-family/superman-kingdomcome.lua
@@ -0,0 +1,29 @@
+return {
+
+  void_black = "#0a0808",
+  kingdom_void = "#0c0a0c",
+  exile_core = "#121014",
+  judgment_glow = "#18121c",
+
+  kingdom_blue = "#113366",
+  elder_blue = "#224477",
+  last_hope_blue = "#335599",
+  restored_blue = "#4466aa",
+
+  kingdom_red = "#880011",
+  mourning_red = "#aa1122",
+  gulag_red = "#cc2233",
+  apocalypse_red = "#ee3344",
+
+  silver_hair = "#bbccee",
+  elder_gold = "#998877",
+  kingdom_gold = "#aa9988",
+  restored_gold = "#bbaa99",
+
+  term_red = "#cc2233",
+  term_blue = "#335599",
+  term_silver = "#bbccee",
+
+  term_red_bright = "#ee3344",
+  term_blue_bright = "#5588cc",
+}
---
+++ lua/prismpunk/palettes/dc/super-family/superman-modern.lua
@@ -0,0 +1,35 @@
+return {
+
+  void_black = "#0a0e18",
+  modern_void = "#0c111c",
+  clark_core = "#121824",
+  fortress_glow = "#182030",
+
+  modern_blue = "#0044cc",
+  superman_blue = "#0055ee",
+  hope_blue = "#0066ff",
+  skyfather_blue = "#3388ff",
+
+  modern_red = "#bb1122",
+  cape_red = "#cc2233",
+  house_of_el_red = "#dd3344",
+  heart_red = "#ff5566",
+
+  shield_gold = "#ffaa00",
+  solar_gold = "#ffbb33",
+  hope_gold = "#ffcc44",
+  sunrise_gold = "#ffdd88",
+
+  fortress_white = "#f0f4ff",
+  smallville_sky = "#dde8ff",
+  lois_cream = "#cce0ff",
+
+  term_blue = "#0055ee",
+  term_red = "#cc2233",
+  term_gold = "#ffbb33",
+  term_cream = "#cce0ff",
+
+  term_blue_bright = "#3388ff",
+  term_red_bright = "#ff6677",
+  term_gold_bright = "#ffdd88",
+}
