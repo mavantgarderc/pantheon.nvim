e1acdf842c54a5713c779986da9d163b6e7ca8cb
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:55:38 +0330

    colorscheme(dc): super-family
    - conner kent
    - dreamer

---
+++ lua/prismpunk/palettes/dc/super-family/conner-kent.lua
@@ -0,0 +1,32 @@
+return {
+
+  void_black = "#0a0c10",
+  lab_night = "#0d1118",
+  kon_core = "#121824",
+  ttk_glow = "#182030",
+
+  ttk_teal = "#00d4aa",
+  kon_teal = "#00e6bb",
+  solar_teal = "#00ffcc",
+  hope_teal = "#66ffee",
+
+  kon_red = "#ff2a44",
+  shield_red = "#ff3355",
+  ttk_red = "#ff4466",
+  rage_red = "#ff6677",
+
+  leather_black = "#111118",
+  steel_gray = "#334455",
+  tactical_gray = "#556677",
+  visor_blue = "#5577aa",
+
+  solar_white = "#eeffff",
+  clone_white = "#ddffff",
+
+  term_teal = "#00e6bb",
+  term_red = "#ff3355",
+  term_white = "#ddffff",
+
+  term_teal_bright = "#66ffee",
+  term_red_bright = "#ff6677",
+}
---
+++ lua/prismpunk/palettes/dc/super-family/dreamer.lua
@@ -0,0 +1,28 @@
+return {
+
+  void_black = "#0c0a18",
+  dream_void = "#0f0d20",
+  precog_core = "#141128",
+  starlight_glow = "#1a1638",
+
+  dream_indigo = "#5522ff",
+  nia_indigo = "#6633ff",
+  precog_violet = "#7744ff",
+  starlight_purple = "#9988ff",
+
+  dreamer_pink = "#ff66dd",
+  nia_pink = "#ff77ee",
+  hope_pink = "#ff88ff",
+  cosmic_pink = "#ffaaee",
+
+  morning_sky = "#aaddff",
+  dream_sky = "#cceeff",
+  nia_white = "#eeffff",
+
+  term_indigo = "#6633ff",
+  term_pink = "#ff77ee",
+  term_sky = "#cceeff",
+
+  term_indigo_bright = "#9988ff",
+  term_pink_bright = "#ffaaff",
+}
