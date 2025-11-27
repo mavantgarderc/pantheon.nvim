eb077571e854b35cdcae34d01c3beeb337f4a840
mavantgarderc <manihabibinava@gmail.com>
2025-11-27 05:55:20 +0330

    colorscheme(dc): super-family
    - clark kent
    - composite superman

---
+++ lua/prismpunk/palettes/dc/super-family/composite-superman.lua
@@ -0,0 +1,40 @@
+return {
+
+  void_black = "#0a0c12",
+  composite_void = "#0d1018",
+  split_core = "#12151f",
+  fracture_glow = "#181c28",
+
+  hope_blue = "#0088ff",
+  composite_blue = "#0099ff",
+  kryptonian_sky = "#00aaff",
+  solar_blue = "#44bbff",
+
+  hope_red = "#dd2233",
+  cape_red = "#ee3344",
+  house_of_el = "#ff4455",
+
+  shield_gold = "#ffaa00",
+  solar_gold = "#ffbb22",
+  kryptonian_sun = "#ffcc44",
+
+  gotham_night = "#0a0f1a",
+  bat_purple = "#6600cc",
+  composite_purple = "#7700dd",
+  fear_violet = "#8800ee",
+  dark_knight = "#9933ff",
+
+  bat_teal = "#00ccaa",
+  utility_teal = "#00ddbb",
+  cape_shadow = "#00eecc",
+
+  term_blue = "#0099ff",
+  term_red = "#ee3344",
+  term_purple = "#7700dd",
+  term_gold = "#ffbb22",
+  term_teal = "#00ddbb",
+
+  term_blue_bright = "#44bbff",
+  term_red_bright = "#ff6677",
+  term_purple_bright = "#aa33ff",
+}
