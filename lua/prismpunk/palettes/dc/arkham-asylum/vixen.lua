901813400377e41ecc8cbf19ea8670865bd5a3d3
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:54:53 +0330

    colorscheme(dc): arkham-asylum-{vixen/white-knight}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/vixen.lua
@@ -0,0 +1,31 @@
+return {
+  void_black = "#0a0807",
+  savanna_night = "#0d0b09",
+  mari_core = "#12100c",
+  totem_glow = "#1f1a14",
+
+  earth_brown = "#8b5e3c",
+  savanna_tan = "#a67c52",
+  tribal_gold = "#c49a6c",
+  sunset_orange = "#ff6b35",
+
+  totem_green = "#00b366",
+  wild_emerald = "#00cc77",
+  spirit_jade = "#00ee99",
+  primal_lime = "#44ffaa",
+
+  totem_blue = "#0088ff",
+  spirit_electric = "#44aaff",
+  animal_surge = "#66ccff",
+
+  blood_orange = "#ff4444",
+  claw_red = "#ee5566",
+
+  term_green = "#00ee99",
+  term_blue = "#44aaff",
+  term_orange = "#ff6b35",
+  term_white = "#f0e6d2",
+
+  term_green_bright = "#66ffbb",
+  term_blue_bright = "#66ddff",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/white-knight.lua
@@ -0,0 +1,29 @@
+return {
+  void_black = "#08090a",
+  asylum_void = "#0a0c0d",
+  bayard_core = "#0d1012",
+  sterile_glow = "#182028",
+
+  knight_white = "#f0f8ff",
+  surgical_white = "#e8f0ff",
+  bleached_bone = "#dde8ff",
+
+  sterile_teal = "#00cccc",
+  medical_cyan = "#00eeff",
+  cold_sterile = "#44ffff",
+
+  scalpel_blue = "#4477aa",
+  operating_room = "#5588cc",
+  frost_steel = "#66aaff",
+
+  purge_red = "#cc2222",
+  martyr_blood = "#ee4444",
+
+  term_cyan = "#00eeff",
+  term_blue = "#66aaff",
+  term_red = "#ee4444",
+  term_white = "#f0f8ff",
+
+  term_cyan_bright = "#66ffff",
+  term_blue_bright = "#88ccff",
+}
