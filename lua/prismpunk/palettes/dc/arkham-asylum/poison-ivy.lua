f451bca3a49c4f094b46b0368f1c5e093208c41c
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:54:00 +0330

    colorscheme(dc): arkham-asylum-{poison-ivy/prometheus}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/poison-ivy.lua
@@ -0,0 +1,29 @@
+return {
+  void_black = "#080c09",
+  greenhouse_night = "#0a110b",
+  isley_core = "#0e1710",
+  vine_glow = "#182d1e",
+
+  ivy_green = "#00aa44",
+  chlorophyll = "#00cc55",
+  emerald_vine = "#00ee77",
+  spring_leaf = "#44ff99",
+
+  orchid_magenta = "#ff22aa",
+  venom_pink = "#ff44cc",
+  lethal_petal = "#ff66ee",
+
+  fertile_soil = "#3a2f22",
+  root_brown = "#554433",
+  bark_brown = "#776644",
+
+  corpse_pale = "#ddeecc",
+  greenhouse_white = "#eeffee",
+
+  term_green = "#00ee77",
+  term_magenta = "#ff66ee",
+  term_white = "#eeffee",
+
+  term_green_bright = "#66ffaa",
+  term_magenta_bright = "#ff88ff",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/prometheus.lua
@@ -0,0 +1,30 @@
+return {
+  void_black = "#0a0808",
+  titan_night = "#0d0b0c",
+  prometheus_core = "#121015",
+  armor_glow = "#1f1822",
+
+  stolen_orange = "#ff4400",
+  titan_flame = "#ff6600",
+  burning_core = "#ff8833",
+  ember_glow = "#ffaa55",
+
+  bat_blue = "#0088ff",
+  ghost_circuit = "#44aaff",
+  cold_vengeance = "#66ccff",
+
+  iron_red = "#aa2222",
+  titan_blood = "#cc3333",
+  wound_red = "#ee5555",
+
+  phantom_white = "#e8f0ff",
+  armor_plating = "#334466",
+
+  term_orange = "#ff6600",
+  term_blue = "#44aaff",
+  term_red = "#cc3333",
+  term_white = "#e8f0ff",
+
+  term_orange_bright = "#ff9944",
+  term_blue_bright = "#66ddff",
+}
