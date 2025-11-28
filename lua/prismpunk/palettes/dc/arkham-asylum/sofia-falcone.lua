12d990da2958c093efa681c6afe65f8ab7d443cd
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:54:26 +0330

    colorscheme(dc): arkham-asylum-{sofia-falcone/solomon-grundy}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/sofia-falcone.lua
@@ -0,0 +1,28 @@
+return {
+  void_black = "#0a0809",
+  gotham_night = "#0c0a0b",
+  falcone_core = "#111013",
+  marble_glow = "#1c181c",
+
+  roman_gold = "#aa8800",
+  empire_gold = "#ccaa22",
+  hangman_gold = "#eecc44",
+  crown_gold = "#ffdd66",
+
+  falcone_red = "#aa1122",
+  lipstick_red = "#cc2233",
+  roman_blood = "#ee3344",
+  arterial_red = "#ff5566",
+
+  marble_white = "#f0e8e8",
+  silk_gray = "#bbaaaa",
+  tombstone_gray = "#887777",
+
+  term_gold = "#eecc44",
+  term_red = "#ee3344",
+  term_gray = "#bbaaaa",
+  term_white = "#f0e8e8",
+
+  term_gold_bright = "#ffdd88",
+  term_red_bright = "#ff7799",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/solomon-grundy.lua
@@ -0,0 +1,33 @@
+return {
+  void_black = "#080a08",
+  swamp_night = "#0a0d0b",
+  grundy_core = "#0e1210",
+  grave_glow = "#18221c",
+
+  rot_green = "#226644",
+  undead_flesh = "#338855",
+  swamp_moss = "#44aa77",
+  resurrection_green = "#55cc99",
+
+  tombstone_gray = "#445555",
+  corpse_gray = "#667777",
+  moon_bone = "#99aaaa",
+
+  death_blue = "#223366",
+  rigor_blue = "#334488",
+  cold_vein = "#4455aa",
+
+  grundy_blood = "#882222",
+  rust_red = "#aa3333",
+
+  moon_sick = "#ddddbb",
+  grave_white = "#eeeecc",
+
+  term_green = "#44aa77",
+  term_blue = "#4455aa",
+  term_red = "#aa3333",
+  term_white = "#eeeecc",
+
+  term_green_bright = "#66cc99",
+  term_blue_bright = "#6677cc",
+}
