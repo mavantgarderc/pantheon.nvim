f6a7e7894592f5af175f14039452a986f82584f6
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:52:49 +0330

    colorscheme(dc): arkham-asylum-{clayface/deathshot}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/clayface.lua
@@ -0,0 +1,32 @@
+return {
+  void_black = "#0a0a0c",
+  clay_night = "#0c0f10",
+  karlo_core = "#111418",
+  ooze_glow = "#182020",
+
+  wet_clay = "#3a2f28",
+  toxic_mud = "#4a3f38",
+  karlo_brown = "#5a4f48",
+  deep_ooze = "#6a5f58",
+
+  corpse_skin = "#d8c8b8",
+  stolen_flesh = "#e8d8c8",
+  pale_mask = "#f0e8e0",
+
+  hagen_green = "#228844",
+  renuyu_toxin = "#33aa66",
+  chemical_slime = "#44cc88",
+  glowing_ooze = "#66eeaa",
+
+  clayface_red = "#882222",
+  oozing_red = "#aa3333",
+
+  term_brown = "#6a5f58",
+  term_green = "#44cc88",
+  term_red = "#aa3333",
+  term_flesh = "#e8d8c8",
+
+  term_brown_bright = "#8a7f78",
+  term_green_bright = "#66eeaa",
+  term_red_bright = "#cc4444",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/deathshot.lua
@@ -0,0 +1,27 @@
+return {
+  void_black = "#08090c",
+  scope_night = "#0b0e12",
+  lawton_core = "#101418",
+  killzone_glow = "#181e22",
+
+  gunmetal = "#2c3238",
+  barrel_steel = "#3a424a",
+  tactical_gray = "#48505a",
+  cold_scope = "#586878",
+
+  red_dot = "#ff0033",
+  contract_red = "#ff1144",
+  killshot_red = "#ff3355",
+  arterial_red = "#ff5577",
+
+  contract_white = "#e8f0ff",
+  sterile_white = "#d8e0f0",
+  target_paper = "#c8d8e8",
+
+  term_red = "#ff3355",
+  term_gray = "#586878",
+  term_white = "#e8f0ff",
+
+  term_red_bright = "#ff5577",
+  term_gray_bright = "#6888a8",
+}
