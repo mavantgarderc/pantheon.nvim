c24e8917042ac9be49c1e0c9893653b2161ad52f
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:54:11 +0330

    colorscheme(dc): arkham-asylum-{riddler/scarecrow}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/riddler.lua
@@ -0,0 +1,26 @@
+return {
+  void_black = "#080c08",
+  arkham_night = "#0a110a",
+  nygma_core = "#0e180e",
+  question_glow = "#1a2d1a",
+
+  riddle_green = "#00cc44",
+  nygma_green = "#00ee66",
+  electric_lime = "#44ff88",
+  enigma_lime = "#88ffbb",
+
+  genius_purple = "#8800ff",
+  riddle_purple = "#aa22ff",
+  intellect_violet = "#cc44ff",
+
+  asylum_white = "#eeffee",
+  steel_gray = "#88aabb",
+
+  term_green = "#00ee66",
+  term_purple = "#cc44ff",
+  term_lime = "#44ff88",
+  term_white = "#eeffee",
+
+  term_green_bright = "#66ff99",
+  term_purple_bright = "#ee66ff",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/scarecrow.lua
@@ -0,0 +1,32 @@
+return {
+  void_black = "#0a0908",
+  asylum_night = "#0d0c0b",
+  crane_core = "#12110f",
+  burlap_glow = "#1e1b17",
+
+  scarecrow_brown = "#6b5522",
+  burlap_brown = "#887733",
+  straw_yellow = "#aa9966",
+  toxin_fog = "#ccbb88",
+
+  fear_gas = "#00cc44",
+  toxin_green = "#22ee66",
+  nightmare_green = "#44ff88",
+
+  terror_purple = "#8800cc",
+  hallucination = "#aa22ee",
+  mindbreak_purple = "#cc44ff",
+
+  rust_red = "#883322",
+  dried_blood = "#aa4433",
+
+  asylum_white = "#e8e8e0",
+
+  term_green = "#22ee66",
+  term_purple = "#cc44ff",
+  term_brown = "#887733",
+  term_white = "#e8e8e0",
+
+  term_green_bright = "#66ffaa",
+  term_purple_bright = "#ee66ff",
+}
