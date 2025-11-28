c3f5d3d94a7a6cd2c14a376a5b61ab9217c1dcfe
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:53:05 +0330

    colorscheme(dc): arkham-asylum-{firefly/gypsy}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/firefly.lua
@@ -0,0 +1,32 @@
+return {
+  void_black = "#0a0808",
+  inferno_night = "#0e0b0b",
+  lynns_core = "#141010",
+  flame_glow = "#221818",
+
+  ignition_orange = "#ff4400",
+
+  blaze_orange = "#ff5500",
+  inferno_orange = "#ff7700",
+  molten_yellow = "#ffaa00",
+
+  jetfuel_green = "#00ff88",
+  phosphorus_green = "#22ffaa",
+  toxic_flame = "#44ffcc",
+
+  charred_black = "#1c1818",
+  ash_gray = "#333030",
+  ember_gray = "#554444",
+
+  plasma_white = "#ffffee",
+  firestorm_white = "#ffeecc",
+
+  term_orange = "#ff7700",
+  term_green = "#44ffcc",
+  term_yellow = "#ffaa00",
+  term_white = "#ffffee",
+
+  term_orange_bright = "#ff9933",
+  term_green_bright = "#66ffee",
+  term_yellow_bright = "#ffcc44",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/gypsy.lua
@@ -0,0 +1,33 @@
+return {
+  void_black = "#0c0912",
+  asylum_night = "#100d18",
+  gypsy_core = "#141122",
+  vision_glow = "#1c1830",
+
+  bruised_violet = "#8800ff",
+  fortune_purple = "#aa22ff",
+  cracked_amethyst = "#cc44ff",
+  mad_vision = "#ee66ff",
+
+  tarot_pink = "#ff2299",
+  fever_dream = "#ff44bb",
+  broken_mirror = "#ff66dd",
+
+  crystal_cyan = "#22eeff",
+  shattered_ice = "#44ccff",
+
+  tarot_red = "#ff1133",
+
+  asylum_gray = "#333348",
+  straitjacket = "#555577",
+
+  term_purple = "#cc44ff",
+  term_pink = "#ff44bb",
+  term_cyan = "#44ccff",
+  term_red = "#ff1133",
+  term_gray = "#555577",
+
+  term_purple_bright = "#ee66ff",
+  term_pink_bright = "#ff66dd",
+  term_cyan_bright = "#66eeff",
+}
