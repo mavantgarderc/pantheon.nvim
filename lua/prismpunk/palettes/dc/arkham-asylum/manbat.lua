3e6940630e66591c3623889c2656f6b37d1b1fa5
mavantgarderc <manihabibinava@gmail.com>
2025-11-28 08:53:45 +0330

    colorscheme(dc): arkham-asylum-{manbat/neojoker}

---
+++ lua/prismpunk/palettes/dc/arkham-asylum/manbat.lua
@@ -0,0 +1,28 @@
+return {
+  void_black = "#08090a",
+  cave_night = "#0b0d10",
+  langstrom_core = "#10131a",
+  wing_glow = "#1a1e28",
+
+  matted_brown = "#443322",
+  beast_brown = "#665544",
+  serum_yellow = "#aa8833",
+  feral_yellow = "#ccaa55",
+
+  bat_red = "#cc1122",
+  rage_red = "#ee3344",
+  blood_moon = "#ff5566",
+
+  sonar_white = "#cceeff",
+  cave_stone = "#446688",
+  ultrasonic_blue = "#66aaff",
+
+  term_red = "#ee3344",
+  term_yellow = "#ccaa55",
+  term_blue = "#66aaff",
+  term_white = "#cceeff",
+
+  term_red_bright = "#ff7799",
+  term_yellow_bright = "#ffdd88",
+  term_blue_bright = "#88ccff",
+}
---
+++ lua/prismpunk/palettes/dc/arkham-asylum/neojoker.lua
@@ -0,0 +1,26 @@
+return {
+  void_black = "#0b080f",
+  ultraviolet_night = "#0f0c14",
+  neo_core = "#131018",
+  circuit_glow = "#21182e",
+
+  neo_purple = "#bb00ff",
+  ultraviolet = "#dd22ff",
+  royal_circuit = "#ee44ff",
+  crown_purple = "#ff66ff",
+
+  venom_green = "#00ff99",
+  joker_lime = "#44ffaa",
+  acid_laugh = "#66ffcc",
+
+  restraint_white = "#e8e8ff",
+  chrome_silver = "#aabbee",
+  neon_blacklight = "#2a1a3a",
+
+  term_purple = "#ee44ff",
+  term_green = "#44ffaa",
+  term_white = "#e8e8ff",
+
+  term_purple_bright = "#ff66ff",
+  term_green_bright = "#88ffdd",
+}
